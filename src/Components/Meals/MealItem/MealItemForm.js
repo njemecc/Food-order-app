2793d335fa11b6bcbf47765e9665cd6678cd21c6
Nima Nekouei <nekoueinima@protonmail.com>
2022-04-06 18:30:13 +0430

    meal item form file created and imported to mealitem

---
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import styles from './MealItemForm.module.css';
+
+const MealItemForm = props => {
+  return (
+    <form className={styles.form}>
+      <input />
+      <button>+ add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
