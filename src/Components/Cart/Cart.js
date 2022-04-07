091b3d172358090e95135ed168840116d819f455
Nima Nekouei <nekoueinima@protonmail.com>
2022-04-07 14:24:23 +0430

    decrease amount section coded

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,8 +10,12 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id => {};
-  const cartItemIncreaseHandler = item => {};
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemIncreaseHandler = item => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={styles['cart-items']}>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,14 +11,12 @@ const cartReducer = (state, action) => {
   if (action.type === 'ADD_ITEM') {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-
     const existingItemIndex = state.items.findIndex(
       item => item.id === action.item.id
     );
     const existingItem = state.items[existingItemIndex];
 
     let updatedItems;
-
     if (existingItem) {
       const updatedItem = {
         ...existingItem,
@@ -35,6 +33,31 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === 'REMOVE_ITEM') {
+    const existingItemIndex = state.items.findIndex(
+      item => item.id === action.id
+    );
+    const existingItem = state.items[existingItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
