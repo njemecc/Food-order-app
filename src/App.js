654eb817b563d7541705bfc05f8bc7918c5e1d0a
Nima Nekouei <nekoueinima@protonmail.com>
2022-04-07 10:44:27 +0430

    cart context and the provider added and replaced with fragment in app component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from '../src/Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
 import Cart from '../src/Components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
